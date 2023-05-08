38c6523231f42a3472b4027c50eaf1b24c054e53
wit03 <witjarukit@gmail.com>
2023-05-09 04:35:31 +0700

    feat: 5 sorting algos

---
+++ .vscode/5Sort/quick.c
@@ -0,0 +1,30 @@
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+// Quicksort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot_index = partition(arr, low, high);
+      
+        quickSort(arr, low, pivot_index - 1);
+        quickSort(arr, pivot_index + 1, high);
+    }
+}
